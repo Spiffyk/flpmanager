06e3f315c7c0c12cb9bdf01a828019099dfa9d69
Oto Šťáva <oto.stava@gmail.com>
2017-05-16 15:48:28 +0200

    Remove getSampleWorkspace

--- Frontend/src/cz/spiffyk/flpmanager/application/Main.java
+++ Frontend/src/cz/spiffyk/flpmanager/application/Main.java
@@ -14,26 +14,6 @@ import javafx.scene.Scene;
 
 public class Main extends Application {
 	
-	private Workspace getSampleWorkspace() {
-		final Workspace workspace = new Workspace("test_workspace");
-		final List<Song> songs = workspace.getSongs();
-		final Song s1 = new Song();
-		s1.setName("Asdf");
-		s1.setAuthor("Foo");
-		songs.add(s1);
-		final Song s2 = new Song();
-		s2.setName("Ghjk");
-		s2.setAuthor("Foo");
-		s2.setFavorite(true);
-		songs.add(s2);
-		final Song s3 = new Song();
-		s3.setName("BB (Foo remix)");
-		s3.setAuthor("Bar");
-		s3.setFavorite(true);
-		songs.add(s3);
-		return workspace;
-	}
-	
 	@Override
 	public void start(Stage primaryStage) {
 		MainScreen mainScreen = new MainScreen();
@@ -43,7 +23,6 @@ public class Main extends Application {
 		stylesheets.add(getClass().getResource("application.css").toExternalForm());
 		stylesheets.add(getClass().getResource("controls/controls.css").toExternalForm());
 		
-//		final Workspace workspace = getSampleWorkspace();
 		try {
 			final Workspace workspace = ManagerFileHandler.loadWorkspace("test_workspace");
 			mainScreen.setWorkspace(workspace);
